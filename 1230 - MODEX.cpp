d34bd227de4e4f16c6a9d49557d3fddcf9cc2554
Tanbin Hasan Ador <tanvin.hasan74@gmail.com>
2021-08-06 13:32:55 +0600

    Create 1230 - MODEX.cpp

---
+++ 1230 - MODEX.cpp
@@ -0,0 +1,31 @@
+/**
+ *    author:  Tanbin_Hasan
+ *    created: 06.08.2021 13:27:08
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int BigMod(int a, int b , int mod) {
+  if (!b) {
+    return 1;
+  }
+  if (b & 1) {
+    return ((a % mod) * BigMod(a, b - 1, mod)) % mod;
+  }
+  int res = BigMod(a, b >> 1, mod);
+  return ((res % mod) * (res % mod)) % mod;
+}
+
+int main(void) {
+  ios::sync_with_stdio(false); cin.tie(0);
+  int t;
+  while (cin >> t && t) {
+    for (int i = 0; i < t; ++i) {
+      int a, b, x;
+      cin >> a >> b >> x;
+      cout << BigMod(a, b, x) << '\n';
+    }
+  }
+  return 0;
+}
