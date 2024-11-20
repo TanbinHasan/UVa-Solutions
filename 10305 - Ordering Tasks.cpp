3f69c30faee8b26dab14bbef21c65f29882b0ced
TanbinHasan <tanvin.hasan74@gmail.com>
2024-11-21 00:07:32 +0600

    test

--- 10305 - Ordering Tasks.cpp
+++ 10305 - Ordering Tasks.cpp
@@ -1,62 +1,58 @@
 #include <bits/stdc++.h>
-
-#define int long long
-
 using namespace std;
 
-vector<int> TopoSorting(vector<vector<int>> &adj, int n) {
-  vector<int> ind(n), ans;
-  for (int i = 0; i < n; ++i) {
-    for (auto &child : adj[i]) {
-      ++ind[child];
-    }
-  }
-  priority_queue<int, vector<int>, greater<int>> pq;
-  for (int i = 0; i < n; ++i) {
-    if (ind[i] == 0) pq.push(i);
-  }
-  int cnt = 0;
-  while (!pq.empty()) {
-    int node = pq.top();
-    pq.pop();
-    ans.push_back(node);
-    for (auto &child : adj[node]) {
-      if (--ind[child] == 0) pq.push(child);
-    }
-    ++cnt;
-  }
-  if (cnt != n) return {};
-  return ans;
-}
+// if the graph isn't acylic topological sorting doesn't exist
+class Graph {
+  int n;
+  vector<vector<int>> g;
+  vector<int> vis, store;
 
-// #define MultipleCase       
-void Solve(__attribute__((unused)) int tc) {
-  int n, m;
-  cin >> n >> m;
-  while (cin >> n >> m) {
-    if (n == 0 && m == 0) break;
-    vector<vector<int>> adj(n);
-    while (m--) {
-      int x, y;
-      cin >> x >> y;
-      --x, --y;
-      adj[x].push_back(y);
+ public:
+  Graph(int n) : n(n), g(n), vis(n, -1) {}
+  void add(int u, int v) { g[u].push_back(v); }
+
+  bool dfs(int u) {
+    vis[u] = 1;
+    for (auto& v : g[u]) {
+      if (vis[v] == 1) return false;
+      if (vis[v] == -1) if (!dfs(v)) return false;
     }
-    auto ans = TopoSorting(adj, n);
-    for (auto &i : ans) {
+    vis[u] = 2;
+    store.push_back(u);
+    return true;
+  }
+
+  vector<int> TopoSorting(void) {
+    for (int i = 0; i < n; ++i) {
+      if (vis[i] != -1) continue;
+      if (!dfs(i)) return {};
+    }
+    reverse(store.begin(), store.end());
+    return store;
+  }
+};
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n, m;
+  while (cin >> n >> m) {
+    if (!n && !m) break;
+    Graph g(n);
+    while (m--) {
+      int u, v;
+      cin >> u >> v;
+      --u, --v;
+      g.add(u, v);
+    }
+    auto ans = g.TopoSorting();
+    for (auto& i : ans) {
       cout << i + 1 << " ";
     }
     cout << '\n';
   }
-}
-
-int32_t main(void) {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt = 1, tc = 0;
-#ifdef MultipleCase
-  cin >> tt;
-#endif
-  while (tt--) Solve(++tc);
   return 0;
 }
+/*
+https://onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1246  
+*/
--- 796 - Critical Links.cpp
+++ 796 - Critical Links.cpp
@@ -1,66 +1,78 @@
-/**
- *    author:  Tanbin_Hasan
- *    created: 14.08.2021 19:48:44
-**/
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<vector<int>> adj;
-vector<bool> vis;
-vector<int> D;
-vector<int> low;
-int cnt;
-set<pair<int,int>> bridge;
+typedef long long i64;
 
-void DFS(int node, int par) {
-  vis[node] = true;
-  D[node] = low[node] = cnt++;
-  for (auto &child : adj[node]) {
-    if (child == par) continue;
-    if (vis[child]) {
-      low[node] = min(low[node], D[child]);
-    } else {
-      DFS(child, node);
-      if (low[child] > D[node]) {
-        bridge.insert({min(child, node) , max(child, node)});
+class Graph {
+  int n;
+  vector<vector<int>> g;
+  // d[] = discovery time, low[] = minimum discovery time among all the nodes in subtree
+  vector<int> d, low;
+  int timer;
+ public:
+  vector<bool> vis;
+  vector<pair<int, int>> bridges;
+  Graph(int n) {
+    this->n = n;
+    g.resize(n);
+    vis.resize(n);
+    d.resize(n);
+    low.resize(n);
+    timer = 0;
+  }
+
+  void add(int u, int v) { g[u].push_back(v); }
+
+  void dfs(int u, int p = -1) {
+    vis[u] = true;
+    d[u] = low[u] = timer++;
+    bool flag = false;
+    for (auto& v : g[u]) {
+      if (v == p && !flag) {
+        flag = true;
+        continue;
       }
-      low[node] = min(low[node], low[child]);
+      if (vis[v]) {  // u -> v is a back edge
+        low[u] = min(low[u], d[v]);
+      } else {  // u -> v is a tree edge
+        dfs(v, u);
+        low[u] = min(low[u], low[v]);
+        if (d[u] < low[v]) { // bridges are identified
+          bridges.push_back({min(u, v), max(u, v)});
+        }
+      }
     }
   }
-}
+};
 
 int main(void) {
-  ios::sync_with_stdio(false); cin.tie(0);
-  int nodes;
-  while (cin >> nodes) {
-    adj.assign(nodes, {});
-    vis.assign(nodes, false);
-    D.assign(nodes, 0);
-    low.assign(nodes, 0);
-    cnt = 0;
-    bridge.clear();
-    for (int i = 0; i < nodes; ++i) {
-      int par, no;
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  while (cin >> n) {
+    Graph g(n);
+    for (int i = 0; i < n; ++i) {
+      int u;
+      cin >> u;
+      int m;
       char ch;
-      cin >> par;
-      cin >> ch >> no >> ch;
-      while (no--) {
-        int x;
-        cin >> x;
-        adj[par].push_back(x);
+      cin >> ch >> m >> ch;
+      while (m--) {
+        int v;
+        cin >> v;
+        g.add(u, v);
       }
     }
-    for (int i = 0; i < nodes; ++i) {
-      if (!vis[i]) {
-        DFS(i, -1);
-      }
+    for (int i = 0; i < n; ++i) {
+      if (!g.vis[i]) g.dfs(i);
     }
-    cout << (int)bridge.size() << " critical links\n";
-    for (auto &i : bridge) {
-      cout << i.first << " - " << i.second << '\n';
+    sort(g.bridges.begin(), g.bridges.end());
+    cout << (int)g.bridges.size() << " critical links\n";
+    for (auto& [u, v] : g.bridges) {
+      cout << u << " - " << v << '\n';
     }
     cout << '\n';
   }
   return 0;
 }
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=9&page=show_problem&problem=737
